1854abe76d849109ef09381238f4635b2ec809d0
Dede Assann <101427923+DedeAssann@users.noreply.github.com>
2023-06-05 20:53:36 +0000

    Nap Vanse!

--- functions&/Turtle_work.py
+++ functions&/Turtle_work.py
@@ -1,19 +1,20 @@
-
+import matplotlib
+matplotlib.use('Agg')
 import turtle
 
 carre = turtle.Turtle()
 
-for i in range(4):
-    carre.fd(200)
-    carre.left(90)
+for i in range(6):
+    carre.fd(202)
+    carre.left(92)
 
 turtle.done()
 
 star = turtle.Turtle()
 
 while True:
-   star.fd(200)
-   star.left(150)
+   star.fd(202)
+   star.left(152)
    if star.pos() == star.position():
     break
 turtle.done()

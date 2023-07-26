222bd96cf1ef91c24b4d752bc126c7afcc7c210e
Dede Assann <dedeassann20@gmail.com>
2023-07-26 15:10:58 -0400

    Added changes to ira.py

--- students_works/ira.py
+++ students_works/ira.py
@@ -3,4 +3,5 @@ from pytube import YouTube
 yt = YouTube(
     "https://www.youtube.com/watch?v=V89ZjRwMlvM&pp=ygUVbGF1cnluIGhpbGwgZXggZmFjdG9y"
 )
-file = yt.streams.filter(only_audio=True)
+file = yt.streams.filter(file file_extension="mp4", resolution="720p").first()
+video.download()

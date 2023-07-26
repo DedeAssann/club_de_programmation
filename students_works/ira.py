307a995ee8615624421c8cccf7fca5200b262836
Cours Prives Edme <edmecoursprives@gmail.com>
2023-07-26 14:58:59 -0400

    Changes

--- students_works/ira.py
+++ students_works/ira.py
@@ -1,5 +1,6 @@
 from pytube import YouTube 
 
 yt = YouTube("https://www.youtube.com/watch?v=V89ZjRwMlvM&pp=ygUVbGF1cnluIGhpbGwgZXggZmFjdG9y")
-file = yt.streams.filter(only_audio=True)
+video = yt.streams.filter(file_extension="mp4", resolution="720p").first()
+video.download()
 
